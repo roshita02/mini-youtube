4e40b4a2b4052f8d71e3c28c90e95c308cdcce6c
roshita02 <roshitashakya07@gmail.com>
2019-12-08 13:51:33 +0545

    Default video selection added

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,21 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 class App extends React.Component{
     state = {videos: [], selectedVideo: null };
+
+    componentDidMount(){
+        this.onTermSubmit('buildings');
+    }
+
     onTermSubmit = async term => {
         const response = await youtube.get('/search/',{
             params: {
                 q: term
             }
         });
-        this.setState({ videos: response.data.items});
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
     onVideoSelect = (video) => {
         this.setState({ selectedVideo: video});
@@ -22,7 +30,7 @@ class App extends React.Component{
                 <SearchBar onFormSubmit={this.onTermSubmit} />
                 <div className="ui grid">
                     <div className="ui row">
-                        <div className="eleven wide column">
+                        <div className="ten wide column">
                             <VideoDetail video={this.state.selectedVideo}/>
                         </div>
                         <div className="five wide column">   
